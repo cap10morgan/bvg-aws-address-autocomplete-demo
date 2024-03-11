ee7f5066dfd52eedecef37eed715da65d865cebc
Wes Morgan <git@wesmorgan.me>
2024-03-11 13:21:16 -0600

    Use correct publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: [path.join(__dirname, "browser.js")],
   output: {
     path: path.resolve(__dirname, 'public'),
-    publicPath: '/',
+    publicPath: '/bvg-aws-address-autocomplete-demo/',
     filename: 'main.js'
   },
   resolve: {
